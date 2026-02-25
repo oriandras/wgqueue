57198cba003af4417fdb20f10c5b2959b51a9140
oriandras <andras.ori@gmail.com>
2026-02-25 10:52:11 +0100

    Enhance scheduling features: add bulk delete, sorting, filters, and pagination. Introduce editable scheduling form with activity logging and dynamic UI updates. Add scheduling edit route.

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,10 @@ Route::get('/scheduling/create', function () {
     return view('scheduling.create');
 })->middleware(['auth']);
 
+Route::get('/scheduling/edit/{id}', function ($id) {
+    return view('scheduling.edit', ['id' => $id]);
+})->name('scheduling.edit')->middleware(['auth']);
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
